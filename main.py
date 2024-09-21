2f009d12cd50fa3a86fb3cd56bac4298d3555d70
Skeyling1 <skeyling1@gmail.com>
2024-09-21 12:54:11 +0300

    final

--- main.py
+++ main.py
@@ -4,35 +4,54 @@
 
 #Класс `User*: Этот класс должен инкапсулировать данные о пользователе: ID, имя и уровень доступа ('user' для обычных сотрудников).
 class User:
-    def __init__(self, staff_id, name):
+    def __init__(self, staff_id, name, access_level = 'user'):
         self.__staff_id = staff_id
         self.__name = name
-        self.__access_level = 'user'
+        self.__access_level = access_level
 
+    def users_getter(self):
+        info = {"id" : self.__staff_id, "name" : self.__name, "access_level" : self.__access_level}
+        print(info)
+
+    def users_setter(self, staff_id, name):
+        self.__staff_id = staff_id
+        self.__name = name
 
 #Класс Admin: Этот класс должен наследоваться от класса User.
 # Добавь дополнительный атрибут уровня доступа, специфичный для администраторов ('admin').
+class Admin(User):
+    def __init__(self, staff_id, name, access_level = 'admin'):
+        super().__init__(staff_id, name, access_level)
+
+    def users_getter(self):
+        super().users_getter()
+
+    def users_setter(self, staff_id, name):
+        super().users_setter(staff_id, name)
+
 # Класс должен также содержать методы add_user и remove_user, которые позволяют добавлять и удалять пользователей из списка
 # (представь, что это просто список экземпляров User).
-class Admin(User):
-    def __init__(self, staff_id, name):
-        super(Admin, self).__init__(staff_id, name)
-        self.__access_level = 'admin'
+    def add_user(self, person):
+        all_stuff_list.append(person)
 
-    def getter(self, users_object):
-        d = (users_object.__staff_id, users_object.__name, users_object.__access_level)
-        print(d)
-
-
-
+    def remove_user(self, person):
+        all_stuff_list.remove(person)
 
 #Инкапсуляция данных: Убедись, что атрибуты классов защищены от прямого доступа и модификации снаружи.
 # Предоставь доступ к необходимым атрибутам через методы (например, get и set методы).
+user1 = User(1, "Derry")              #создали юзера
+admin1 = Admin(77777777777, "Marie")   #создали админа
+admin1.users_getter()                 #вывод данных
+user1.users_getter()                 #вывод данных
+admin1.users_setter(222, "Ivan")    #замена данных
+user1.users_setter(333, "Ivan")    #замена данных
+admin1.users_getter()                #вывод изменненных данных
+user1.users_getter()                #вывод изменненных данных
 
-user1 = User(1, "Derry")
-
-admin1 = Admin(77777777777, "Marie")
-
-admin1.getter(user1)
-
-#print(user1.__name)
+all_stuff_list = []
+admin1.add_user(user1)  #функция добавления
+admin1.add_user(user1)  #функция добавления
+admin1.add_user(admin1)  #функция добавления
+print(all_stuff_list)
+admin1.remove_user(user1)  #удаление лишней записи
+print(all_stuff_list)
